0bcfe2c440788d391defd9bd018da6ca7d310016
Matthew Damron <mattdamron@msn.com>
2021-04-10 11:18:36 -0600

    add scemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(
+      bookAuthor: [String]
+      description: String!
+      title: String!
+      bookId: String!
+      iamge: String!
+      link: String!
+    ): User
+    removeBook(bookId: String!): User
+  }
 `;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
-const express = require('express');
 const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
+const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -10,9 +8,9 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const app = express();
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -22,23 +20,18 @@ const server = new ApolloServer({
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
-// app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+};
 
-// app.use(routes);
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
